d395923c3c74d75370f6def03f64e437c4cd3b50
prasad.s <prasadjshimpi@gmail.com>
2025-05-22 17:45:36 +0530

    local grid setup for run

---
+++ src/test/java/examples/GridHeadlessTest2.java
@@ -0,0 +1,25 @@
+package examples;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class GridHeadlessTest2 {
+    
+	@Test()
+	public void checkBrowser() throws MalformedURLException {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new"); // Use "--headless=new" for modern headless
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+        driver.get("https://www.facebook.com");
+
+        System.out.println("Title: " + driver.getTitle());
+        driver.quit();
+    }
+}
